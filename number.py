4c4dcb5ab55a94326bc7305178f719a56b2fc6d0
Adeshina <adeshiname@gmail.com>
2023-05-24 18:26:56 +0100

    list

---
+++ number.py
@@ -0,0 +1,14 @@
+from math import *
+
+
+number1 = 1
+number = 78
+print(78+28)
+print(str(number1))
+
+print(abs(-5))
+print(max(4, 2,5, 8))
+print(min(4, 2,5, 8))
+print(round(3.2))
+print(bin(334)) # prints binary form of a value
+
