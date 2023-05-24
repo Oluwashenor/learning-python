7cb6447b8b571382cbc96b2dcb8ec27d4ab6d026
Adeshina <adeshiname@gmail.com>
2023-05-24 17:25:26 +0100

    Initail

---
+++ app.py
@@ -0,0 +1,12 @@
+name = 'Adesina'
+age = 5000
+
+print('Hello World')
+print('Welcome',name)
+print(name + 'is my name') # Concatenation
+print(name + 'is my name', age) 
+print('Hi, \nHow are you') # \n new line
+print(name[0])
+print(name.upper())
+print(name.upper())
+ 
