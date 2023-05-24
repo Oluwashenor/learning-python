e70ff30e6de4e80be67852fcd932e6fc8a25081b
Adeshina <adeshiname@gmail.com>
2023-05-24 18:27:12 +0100

    list, number

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ print(name + 'is my name', age)
 print('Hi, \nHow are you') # \n new line
 print(name[0])
 print(name.upper())
-print(name.upper())
- 
+print(name.lower())
+print(name.islower())
+print(name.isupper())
+print(len(name)) # Length of a string
+print(name.index('a')) # find the index of value
+print(name.replace('a', 'q')) # replace former with latter
+
+
+name = input('Whats your name:')
+print(name)
---
+++ pythonlist.py
@@ -0,0 +1,17 @@
+countries = ['United Kindgom', 'Ghana', 'Nigeria', 'Australia']
+print(countries)
+print(countries[0]) # First item in list
+print(countries[0][1]) # second charcter in first item  in list 
+print(countries[1:]) # shows from index 1 to the end
+print(countries[1:2]) # range
+
+print(type(countries))
+
+#you can have various types in a list 
+
+
+print(countries[-1]) # First item in list from reverse
+
+newList = list(('Nigeria', 34, False))
+
+countries[0] = 'United States' # Change Value
